61040df7d6040775888dd8845a13fb56e0aa1088
Thaynara Santos <thaynaracsantos@gmail.com>
2023-04-02 04:14:36 -0300

    Fix navbar

--- front/pages/guide.js
+++ front/pages/guide.js
@@ -4,7 +4,7 @@ export default function Guide() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-pink-900 dark:text-pink-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Guida de Uso
+            Guia de Uso
           </h1>
         </div>
         <div className="container py-12 ">
