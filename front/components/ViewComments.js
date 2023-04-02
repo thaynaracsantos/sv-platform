4ce6781080c830c2c42f52b88f0176a54342b23f
marcinhojazz <marcinhojazz@gmail.com>
2023-04-02 00:57:53 -0300

    components containercollapse & viewcomments

---
+++ front/components/ViewComments.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContainerCollapse from './ContainerCollapse'
+
+function ViewComments() {
+  return (
+    <>
+      <ContainerCollapse title={''}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          sodales laoreet ligula, vel fermentum magna pharetra a. Cras
+          tristique neque in mi fringilla, a feugiat urna consequat.
+        </p>
+      </ContainerCollapse>
+    </>
+  )
+}
+
+export default ViewComments
