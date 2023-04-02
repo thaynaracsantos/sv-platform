f2d6e94821b9deac8d2ec992f8311951a843deac
Thaynara Santos <thaynaracsantos@gmail.com>
2023-04-02 04:10:07 -0300

    Fix navbar

--- front/data/headerNavLinks.js
+++ front/data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
-  { href: '/new-post', title: 'Desabafe' },
   { href: '/guide', title: 'Guida de Uso' },
+  { href: '/new-post', title: 'Desabafe!' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Parceiros' },
   { href: '/about', title: 'Sobre' },
